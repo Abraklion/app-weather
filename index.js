c53f93e626491732f14c90b240a8808df655b888
Abraklion <abraklion@gmail.com>
2021-10-08 16:48:10 +0500

    Урок 6. NodeJS. Обработка формы

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // подключаем модуль
 let express = require('express');
+let bodyParser = require('body-parser');
 
 // запускаем модуль
 let app = express();
@@ -8,15 +9,30 @@ let app = express();
 app.set('view engine', 'ejs');
 // устанавливаем путь к статическим файлам
 app.use(express.static('public'));
+// настройка для того что бы парсить данные в req.body
+app.use(bodyParser.urlencoded({extended : true}))
 
 // запросы HTTP GET по указанному пути с указанными функциями обратного вызова.
-app.get('/', (reg, res) => {
+app.get('/', (req, res) => {
 
   // отдавем визуальный вид клиенту (HTML).
   res.render('index');
 
   // завершает процесс ответа.
   // res.end('Hello');
+})
+
+// запросы HTTP POST по указанному пути с указанными функциями обратного вызова. (для отправки данных формы)
+app.post('/', (req, res) =>{
+
+  // деструктурируем обьект req.body
+  let { city } = req.body;
+
+  // выводим названия города в консоль
+  console.log(city);
+
+  // отдавем визуальный вид клиенту (HTML).
+  res.render('index');
 })
 
 // запискаем сервер на порту 3000
