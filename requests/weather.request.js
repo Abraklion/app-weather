9e28168287a46735fd5284c8fac9fa9994470d91
Abraklion <abraklion@gmail.com>
2021-10-11 12:09:31 +0500

    Урок 9. NodeJS. Получение погоды

--- requests/weather.request.js
+++ requests/weather.request.js
@@ -9,6 +9,44 @@ module.exports = async function(city = ''){
     throw new Error("Имя города не может быть пустым")
   }
 
-  // выводим город
-  console.log('City:', city);
+  // Api ключ
+  const KEY = '828451a13a1cbf889f1ea0fc17944516';
+  const uri = `http://api.openweathermap.org/data/2.5/weather`;
+
+  // параметры запроса, беруться из request-promise : https://www.npmjs.com/package/request-promise
+  const options = {
+    uri,
+    qs:{
+      appid: KEY,
+      q: city,
+      units: 'imperial'
+    },
+    json: true
+  }
+
+  try {
+
+    // запрос по api к приложению с погодой
+    const data = await rp(options);
+
+    // формула переводит кельвины в цельсиа
+    const celsius = ((data.main.temp - 32) * 5/9).toFixed(0);
+
+    // возвращаем объект
+    return {
+      weather: `${data.name}: ${celsius}`,
+      error: null
+    }
+
+  } catch (err) {
+
+    // возвращаем объект
+    return {
+      weather: null,
+      error: err.message
+    }
+
+  }
+
+
 }
